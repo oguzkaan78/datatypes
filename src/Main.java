bae19c139ed48b06b348cefc6e6a568ae7f04ae7
oguzkaan78 <gezginler78@hotmail.com>
2022-03-31 20:31:41 +0300

    datatypess

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // String = metni belirtmek için kullanırız
+        // double = ondalıklı sayıları belirtmek için kullanırız
+        // int = normal sayiları belirtmek için kullanırız
+        // char = tek karakteri belirtmek için kullanırız
+        // boolean = true veya false işlemleri belirtirken kullanırız
+
+        String metin = "metinifadesi";
+        double ondalikliSayi = 135.69;
+        int normalSayi = 135;
+        char tekKarakter = 'A';
+        boolean dogrumuyanlismi = true;
+
+    }
+}
